b2204715a44e7b18d9a50c1a40858e63a840be40
Jaydeep <jdsenwarkr@gmail.com>
2024-08-01 19:18:52 +0530

    day 5

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
-import Clients from "@/components/Clients"
+import Clients from "@/components/"
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
